8e0938070b796949c83f6216a3b91363181bfa24
gassara-kys <ogasawara_kiyoshi@cybernagent>
2023-10-13 23:38:00 +0900

    fix: required

--- module/gcp-scan-setting/variables.tf
+++ module/gcp-scan-setting/variables.tf
@@ -1,21 +1,18 @@
 variable "google_cloud_project_id" {
   type        = string
   description = "Your Google Cloud Project ID"
-  default     = "your-project-id"
   nullable    = false
 }
 
 variable "google_cloud_project_name" {
   type        = string
   description = "Your Google Cloud Project Name"
-  default     = "your-project-name"
   nullable    = false
 }
 
 variable "risken_service_account" {
   type        = string
   description = "ServiceAccount of RISKEN"
-  default     = "risken@risken-project.iam.gserviceaccount.com"
   nullable    = false
 }
 
@@ -28,6 +25,5 @@ variable "risken_custom_role_name" {
 variable "risken_verification_code" {
   type        = string
   description = "Verification code of RISKEN"
-  default     = "xxxxxxxxx"
   nullable    = false
 }
